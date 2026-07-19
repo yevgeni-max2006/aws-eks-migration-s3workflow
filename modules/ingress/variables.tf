6a2d8e0b40871b6860b64e363e9b41ef9e55d31e
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-07-19 13:24:44 +0300

    Create variables.tf

---
+++ modules/ingress/variables.tf
@@ -0,0 +1,25 @@
+
+
+variable "name" {
+  description = "The name of the service/ingress"
+  type        = string
+  default     = "velero-ui"
+}
+
+variable "namespace" {
+  description = "Namespace where the ingress and service are deployed"
+  type        = string
+  default     = "velero-ui"
+}
+
+variable "host" {
+  description = "Hostname for ingress"
+  type        = string
+  default     = "backup-dev.appflex.io"
+}
+
+variable "service_port" {
+  description = "Port of the backend service"
+  type        = number
+  default     = "3000"
+}
