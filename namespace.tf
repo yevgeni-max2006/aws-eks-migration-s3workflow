c12fb7f6f74fe73aa7ebbfbfcaa9083581be1342
app-yevgeni <23049337+yevgeni-max2006@users.noreply.github.com>
2026-07-24 22:26:42 +0300

    Update namespace.tf

--- namespace.tf
+++ namespace.tf
@@ -1,3 +1,5 @@
+
+
 resource "kubernetes_namespace" "migration" {
   metadata {
     name = "migration"
@@ -10,4 +12,41 @@ resource "kubernetes_namespace" "migration" {
     prevent_destroy = false
     ignore_changes = all
   }
+}
+
+resource "kubernetes_namespace" "velero" {
+  depends_on = [
+    kubernetes_namespace.migration
+  ]
+  metadata {
+    name = "velero"
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes = all
+  }
+}
+
+resource "kubernetes_secret" "velero_cloud_credentials" {
+  depends_on = [
+    kubernetes_namespace.velero
+  ]
+  metadata {
+    name      = "cloud-credentials"
+    namespace = "velero"
+  }
+
+  data = {
+    cloud = <<-EOF
+[default]
+aws_access_key_id=${var.aws_access_key_id}
+aws_secret_access_key=${var.aws_secret_access_key}
+EOF
+  }
+
+  type = "Opaque"
 }
